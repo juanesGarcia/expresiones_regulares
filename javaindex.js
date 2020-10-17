a57ea93916c36db24ba709c19ea07037b864ac43
juanes <juanesgym2018@gmail.com>
2020-10-17 18:22:01 -0500

    fin proyecto

--- javaindex.js
+++ javaindex.js
@@ -1,11 +1,10 @@
-function capturar(){
+function capturar() {
   ca = document.getElementById("ex").value;
-  regEx = /^[A-Z]{1}[0-9]{6}[a-z A-Z]{3}\W{3}$/;
+  regEx = /^[A-Z]{1}[0-9]{3}[a-z A-Z]{3}\W{3}$/;
   console.log(regEx.test(ca));
-  if(regEx.test(ca)==true){
-      
-  }else{
-
+  if (regEx.test(ca) == true) {
+    $('#dataCorrect').modal('show');
+  } else {
+    $('#incorrectData').modal('show');
   }
-
 }
